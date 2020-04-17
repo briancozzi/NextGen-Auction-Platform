04bc2126ddcdd4ad6ba0296cbad5e60ae74ad17a
alpeshkalena <alpeshkalena@outlook.com>
2020-04-17 20:01:53 +0530

    added event and address entity

---
+++ aspnet-core/src/NextGen.BiddingPlatform.Core/AddressEntity/Address.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Authorization.Users;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace NextGen.BiddingPlatform.AddressEntity
+{
+    [Table("Addresses")]
+    public class Address : AuditedEntity<Guid>
+    {
+        [Required]
+        [MaxLength(AbpUserBase.MaxUserNameLength)]
+        public string Address1 { get; set; }
+
+        [MaxLength(AbpUserBase.MaxUserNameLength)]
+        public string Address2 { get; set; }
+
+        [Required]
+        [MaxLength(25)]
+        public string City { get; set; }
+
+        [ForeignKey("State")]
+        public Guid StateId { get; set; }
+        public State State { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public string ZipCode { get; set; }
+    }
+}
---
+++ aspnet-core/src/NextGen.BiddingPlatform.Core/Events/Event.cs
@@ -0,0 +1,33 @@
+﻿using Abp.Authorization.Users;
+using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using NextGen.BiddingPlatform.AddressEntity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NextGen.BiddingPlatform.Events
+{
+    [Table("Events")]
+    public class Event : FullAuditedEntity<Guid>, IMustHaveTenant
+    {
+        public int TenantId { get; set; }
+
+        [ForeignKey("AppAccount")]
+        public Guid AppAccountId { get; set; }
+
+        [Required]
+        [MaxLength(AbpUserBase.MaxUserNameLength)]
+        public string EventName { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public DateTime EventStartDateTime { get; set; }
+        public DateTime EventEndDateTime { get; set; }
+
+        [ForeignKey("Address")]
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+    }
+}
