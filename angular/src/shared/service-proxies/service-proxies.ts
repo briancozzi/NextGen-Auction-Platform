e9125f84f224137393328c840176548cdfaa1a06
Subhash Kumar <subhashkmrchoudhary2017@gmail.com>
2020-05-04 20:58:47 +0530

    update changes

--- angular/src/shared/service-proxies/service-proxies.ts
+++ angular/src/shared/service-proxies/service-proxies.ts
@@ -805,6 +805,57 @@ export class AccountEventServiceProxy {
     /**
      * @return Success
      */
+    test(): Observable<string> {
+        let url_ = this.baseUrl + "/api/services/app/AccountEvent/Test";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",			
+            headers: new HttpHeaders({
+                "Accept": "text/plain"
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processTest(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processTest(<any>response_);
+                } catch (e) {
+                    return <Observable<string>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<string>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processTest(response: HttpResponseBase): Observable<string> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = resultData200 !== undefined ? resultData200 : <any>null;
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<string>(<any>null);
+    }
+
+    /**
+     * @return Success
+     */
     getAllAccountEvents(): Observable<ListResultDtoOfAccountEventListDto> {
         let url_ = this.baseUrl + "/api/services/app/AccountEvent/GetAllAccountEvents";
         url_ = url_.replace(/[?&]$/, "");
@@ -18033,7 +18084,7 @@ export class CreateAccountEventDto implements ICreateAccountEventDto {
     email!: string | undefined;
     mobileNo!: string | undefined;
     eventUrl!: string | undefined;
-    timeZone!: string | undefined;
+    timeZone!: string;
     isActive!: boolean;
     address!: AddressDto;
 
@@ -18095,7 +18146,7 @@ export interface ICreateAccountEventDto {
     email: string | undefined;
     mobileNo: string | undefined;
     eventUrl: string | undefined;
-    timeZone: string | undefined;
+    timeZone: string;
     isActive: boolean;
     address: AddressDto;
 }
@@ -18108,7 +18159,7 @@ export class UpdateAccountEventDto implements IUpdateAccountEventDto {
     email!: string | undefined;
     mobileNo!: string | undefined;
     eventUrl!: string | undefined;
-    timeZone!: string | undefined;
+    timeZone!: string;
     isActive!: boolean;
     address!: AddressDto;
 
@@ -18170,7 +18221,7 @@ export interface IUpdateAccountEventDto {
     email: string | undefined;
     mobileNo: string | undefined;
     eventUrl: string | undefined;
-    timeZone: string | undefined;
+    timeZone: string;
     isActive: boolean;
     address: AddressDto;
 }
