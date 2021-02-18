f0c2b2231318b73201ac9b881f06924caf7221de
Atul Sharma <Atul Sharma@ATUL>
2021-02-18 16:09:24 +0530

    wrong commit

---
+++ aspnet-core/src/NextGen.BiddingPlatform.Application.Shared/DynamicEntityProperties/Dto/GetAllEntitiesHasDynamicPropertyOutput.cs
@@ -0,0 +1,7 @@
+﻿namespace NextGen.BiddingPlatform.DynamicEntityProperties.Dto
+{
+    public class GetAllEntitiesHasDynamicPropertyOutput
+    {
+        public string EntityFullName { get; set; }
+    }
+}
---
+++ aspnet-core/test/NextGen.BiddingPlatform.Tests/Common/DtoSortingHelper_Tests.cs
@@ -0,0 +1,41 @@
+﻿using NextGen.BiddingPlatform.Common;
+using Shouldly;
+using Xunit;
+
+namespace NextGen.BiddingPlatform.Tests.Common
+{
+    public class DtoSortingHelper_Tests
+    {
+        [Fact]
+        public void ReplaceSorting_Test()
+        {
+            var sorting = "UserName desc, Age asc";
+
+            DtoSortingHelper.ReplaceSorting(sorting, s =>
+            {
+                if (s == "UserName desc")
+                {
+                    s = "username desc";
+                }
+
+                if (s == "Age asc")
+                {
+                    s = "age asc";
+                }
+
+                return s;
+            }).ShouldBe("username desc,age asc");
+
+            sorting = "UserName desc";
+
+            DtoSortingHelper.ReplaceSorting(sorting, s =>
+            {
+                if (s == "UserName desc")
+                {
+                    s = "username desc";
+                }
+                return s;
+            }).ShouldBe("username desc");
+        }
+    }
+}
